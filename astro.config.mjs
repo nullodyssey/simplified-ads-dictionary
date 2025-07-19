b116022c5ce8ff9db7c7bd5ce6b16456fa0a4c12
jjfufu <josuahfustier@hotmail.fr>
2025-07-19 18:31:54 +0200

    feat: setup astro with starlight theme

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightThemeObsidian from 'starlight-theme-obsidian'
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			plugins: [starlightThemeObsidian()],
+			title: 'SimplyAds - Dictionary',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/nullodyssey/simplified-ads-dictionary' }],
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Example Guide', slug: 'guides/example' },
+					],
+				},
+				{
+					label: 'Reference',
+					autogenerate: { directory: 'reference' },
+				},
+			],
+		}),
+	],
+});
